28327a50029e3a61ec9f818e59acaaa02c7e1f88
jolinboy <atti.alive@gmail.com>
2014-04-05 02:02:20 +0900

    Update matrix.java

--- matrix.java
+++ matrix.java
@@ -1,11 +1,22 @@
-public int matrix(int a, int b, int c, int d, int count) {
-a=a*a+b*c;
-b=a*b+b*d;
-c=a*c+c*d;
-d=b*c+d*d;
-count -= count;
-if(count==0) {
-System.out.println("A : "+a+"B : "+b+"C : "+c+"D : "+d);
+class matrixtest {
+public static int oa=2;
+public static int ob=-1;
+public static int oc=4;
+public static int od=-1;
+public static int ocount=21;
+public static void main(String args[]) {
+matrix(oa, ob, oc, od, ocount);
+}
+public static void matrix(int a, int b, int c, int d, int count) {
+int ua, ub, uc, ud, ucount;
+ua=(a*2)+(b*4);
+ub=(a*-1)+(b*-1);
+uc=(c*2)+(d*4);
+ud=(c*-1)+(d*-1);
+ucount=count-1;
+if(count==1) {
+System.out.println("A : "+a+" | "+"B : "+b+" | "+"C : "+c+" | "+"D : "+d);
 return; }
-matrix(a, b, c, d, count); }
-
+matrix(ua, ub, uc, ud, ucount);
+}
+}
